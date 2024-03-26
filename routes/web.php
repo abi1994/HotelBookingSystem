b9e827dcb6b6a081a15e4972968de7356dbd9caa
Abirame <abirame.ceymplon@jeylabs.com.au>
2024-03-26 15:22:52 +0530

    adding files

---
+++ routes/web.php
@@ -0,0 +1,76 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RoomtypeController;
+use App\Http\Controllers\RoomController;
+use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\BookingController;
+use App\Http\Controllers\ServiceController;
+use App\Http\Controllers\BannerController;
+use App\Http\Controllers\BotManController;
+
+use App\Http\Controllers\HomeController;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/',[HomeController::class,'home']);
+Route::get('/service/{id}',[HomeController::class,'service_detail']);
+
+// Admin Dashboard
+Route::get('admin',[AdminController::class,'dashboard']);
+
+// Banner Routes
+Route::get('admin/banner/{id}/delete',[BannerController::class,'destroy']);
+Route::resource('admin/banner',BannerController::class);
+
+// Admin Login
+Route::get('admin/login',[AdminController::class,'login']);
+Route::post('admin/login',[AdminController::class,'check_login']);
+Route::get('admin/logout',[AdminController::class,'logout']);
+
+// RoomType Routes
+Route::get('admin/roomtype/{id}/delete',[RoomtypeController::class,'destroy']);
+Route::resource('admin/roomtype',RoomtypeController::class);
+
+// Room
+Route::get('admin/rooms/{id}/delete',[RoomController::class,'destroy']);
+Route::resource('admin/rooms',RoomController::class);
+
+// Customer
+Route::get('admin/customer/{id}/delete',[CustomerController::class,'destroy']);
+Route::resource('admin/customer',CustomerController::class);
+
+// Delete Image
+Route::get('admin/roomtypeimage/delete/{id}',[RoomtypeController::class,'destroy_image']);
+
+// Booking
+Route::get('admin/booking/{id}/delete',[BookingController::class,'destroy']);
+Route::get('admin/booking/available-rooms/{checkin_date}',[BookingController::class,'available_rooms']);
+Route::resource('admin/booking',BookingController::class);
+
+Route::get('login',[CustomerController::class,'login']);
+Route::post('customer/login',[CustomerController::class,'customer_login']);
+Route::get('register',[CustomerController::class,'register']);
+Route::get('logout',[CustomerController::class,'logout']);
+
+Route::get('booking',[BookingController::class,'front_booking']);
+Route::get('booking/success',[BookingController::class,'booking_payment_success']);
+Route::get('booking/fail',[BookingController::class,'booking_payment_fail']);
+
+// Service CRUD
+Route::get('admin/service/{id}/delete',[ServiceController::class,'destroy']);
+Route::resource('admin/service',ServiceController::class);
+
+// BotMan
+Route::match(['get','post'],'/botman', [BotManController::class, 'handle']);
